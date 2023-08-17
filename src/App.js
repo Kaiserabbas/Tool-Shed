cdbcaeb41b6fe8e7d93eed8ab91e246202eb8214
Qaisar <kayser.abbas@gmail.com>
2023-08-17 21:59:17 +0500

    Modify App.js for new components and add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Headline from './components/Headline';
+import FetchActivity from './components/FetchActivity';
+import FetchIP from './components/FetchIP';
+import QuoteDisplay from './components/QuoteDisplay';
+import RandomPasswordGenerator from './components/GeneratePassword';
+import WordDefinitions from './components/Dictionary';
+import './components/App.css';
 
 function App() {
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav">
+        <Link className="nav-link active" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="fetchActivity">
+          Random Activity Challenge
+        </Link>
+        <Link className="nav-link" to="fetchIp">
+          Find Your IP
+        </Link>
+        <Link className="nav-link" to="quote">
+          Random Quote
+        </Link>
+        <Link className="nav-link" to="randomPassword">
+          Generate Random Passwords
+        </Link>
+        <Link className="nav-link" to="dictionary">
+          {' '}
+          Dictionary{' '}
+        </Link>
+        <button className="refresh-button" onClick={refreshPage}>
+          <i className="material-icons">refresh</i> Refresh
+        </button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Headline />} />
+        <Route path="/fetchActivity" element={<FetchActivity />} />
+        <Route path="/fetchIp" element={<FetchIP />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+        <Route path="/randomPassword" element={<RandomPasswordGenerator />} />
+        <Route path="/dictionary" element={<WordDefinitions />} />
+      </Routes>
+    </>
   );
 }
 
