b4bd128900d317b3a96db795fcc6904c0df72f7c
Qaisar <kayser.abbas@gmail.com>
2023-08-17 22:00:03 +0500

    Modify index.js for BrowserRoute

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
