0ddefe91c69bc29649bdb89e96a246358ac9540d
Qaisar <kayser.abbas@gmail.com>
2023-08-18 14:35:00 +0500

    Modify navbar list

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import FetchIP from './components/FetchIP';
 import QuoteDisplay from './components/QuoteDisplay';
 import RandomPasswordGenerator from './components/GeneratePassword';
 import WordDefinitions from './components/Dictionary';
+import WeatherComponent from './components/Weather';
 import ZipCodeInfo from './components/ZipCode';
+import ContactPage from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import logo from './assets/logo.png';
 import './components/App.css';
 
 function App() {
@@ -16,33 +21,46 @@ function App() {
 
   return (
     <>
-      <nav className="nav">
-        <Link className="nav-link active" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="fetchActivity">
-          Random Activity Challenge
-        </Link>
-        <Link className="nav-link" to="fetchIp">
-          Find Your IP
-        </Link>
-        <Link className="nav-link" to="quote">
-          Random Quote
-        </Link>
-        <Link className="nav-link" to="randomPassword">
-          Generate Random Passwords
-        </Link>
-        <Link className="nav-link" to="dictionary">
-          {' '}
-          Dictionary{' '}
-        </Link>
-        <Link className="nav-link" to="zipCode">
-          Zipcode Information
-        </Link>
+      <Navbar bg="light" expand="lg" className="navbar">
         <button className="refresh-button" onClick={refreshPage}>
           <i className="material-icons">refresh</i> Refresh
         </button>
-      </nav>
+        <Navbar.Brand as={Link} to="/" className="nav-brand">
+          Home
+        </Navbar.Brand>
+        <Link to="/contact" className="contact">
+          Contact
+        </Link>
+        <Navbar.Toggle aria-controls="navbarNavDropdown" />
+        <Navbar.Collapse id="navbarNavDropdown">
+          <Nav className="mr-auto">
+            <NavDropdown title="Find Tools" id="navbarNavDropdown">
+              <NavDropdown.Item as={Link} to="/fetchActivity">
+                Random Activity Challenge
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/fetchIp">
+                Find Your IP
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/quote">
+                Amazing Quotes
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/randomPassword">
+                Random Password Generator
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/dictionary">
+                Dictionary
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/weather">
+                Find Weather Updates
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/zipCode">
+                ZipCode Details
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <img src={logo} alt="Logo" className="logo" />
+      </Navbar>
       <Routes>
         <Route path="/" element={<Headline />} />
         <Route path="/fetchActivity" element={<FetchActivity />} />
@@ -50,7 +68,9 @@ function App() {
         <Route path="/quote" element={<QuoteDisplay />} />
         <Route path="/randomPassword" element={<RandomPasswordGenerator />} />
         <Route path="/dictionary" element={<WordDefinitions />} />
+        <Route path="/weather" element={<WeatherComponent />} />
         <Route path="/zipCode" element={<ZipCodeInfo />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </>
   );
